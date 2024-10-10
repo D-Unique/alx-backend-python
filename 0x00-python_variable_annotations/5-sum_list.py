ba5b62f8ae561c7d0f219fbd1c75ab9ddce07282
Unigwe <emmunigwe@gmail.com>
2024-10-10 19:33:53 +0100

    feat(5-sum_list.py): added a function

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module contains a funtion sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """this function accept a list of float
+    returns a sum of the list"""
+    return sum(input_list)
