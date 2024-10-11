ab4acf8cff4c08b15b077948e98203d912c21c0b
Unigwe <emmunigwe@gmail.com>
2024-10-11 01:06:24 +0100

    fix(9-element_length.py): added docstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,4 +7,5 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """this function returns a list"""
     return [(i, len(i)) for i in lst]
